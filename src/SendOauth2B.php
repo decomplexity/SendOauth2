0ac1c4a063eccd1ec77310c2c49938186fa23522
decomplexity <65123375+decomplexity@users.noreply.github.com>
2021-03-13 20:05:45 +0000

    Update SendOauth2B.php

--- src/SendOauth2B.php
+++ src/SendOauth2B.php
@@ -4,7 +4,7 @@
  * SendOauth2B Wrapper For Microsoft and Google OIDC/OAUTH2 For PHPMailer
  * PHP Version 5.5 and greater
  *
- * @version  1.0.0
+ * @version  1.0.3
  * @category Class
  * @see      https://github.com/PHPMailer/PHPMailer/ The PHPMailer GitHub project
  * @author   Max Stewart (decomplexity) <SendOauth2@decomplexity.com>
